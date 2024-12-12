99b780e7503a244f34999179e8bfb9676f46428d
whdjh <whdjh98@naver.com>
2024-12-12 22:05:04 +0900

    ⚰️ Index.tsx에서 테스트 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,3 @@
 export default function Home() {
-  return (
-    <div
-      style={{
-        backgroundColor: 'var(--violet-main)',
-        color: 'var(--violet-light)',
-        padding: '2rem',
-        fontSize: '1.6rem',
-      }}
-    >
-      중급 프로젝트-taskify
-    </div>
-  );
+  return <>중급 프로젝트-taskify</>;
 }

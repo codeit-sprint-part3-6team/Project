decc870dd4513bd391322e38a69b57985416bb72
twtwkim <alfog23@gmail.com>
2024-12-12 17:24:58 +0900

    ✨(공통컴포넌트) : 제목, 태그 인풋 완성성

---
+++ src/components/common/input/tag-input.tsx
@@ -0,0 +1,13 @@
+import styles from './tag-input.module.css';
+
+export default function TagInput() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.box}>
+        <p className={styles[`box-title`]}>태그</p>
+        <p className={styles[`box-dot`]}>*</p>
+      </div>
+      <input className={styles.input} placeholder="입력 후 Enter" />
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import CommentInput from '@/components/common/input/comment-input';
+import TagInput from '@/components/common/input/tag-input';
+import TitleInput from '@/components/common/input/title-input';
 
 export default function Home() {
   return (
     <>
       중급 프로젝트-taskify
       <CommentInput />
+      <TitleInput />
+      <TagInput />
     </>
   );
 }

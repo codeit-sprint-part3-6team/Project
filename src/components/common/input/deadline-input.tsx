c627426262a3f93bec3b87001235f4a174874226
twtwkim <alfog23@gmail.com>
2024-12-13 13:44:30 +0900

    ✨ 마감일 인풋 완성(Mui date time picker 사용)

--- src/components/common/input/deadline-input.tsx
+++ src/components/common/input/deadline-input.tsx
@@ -7,19 +7,52 @@ import { styled } from '@mui/system';
 import styles from './deadline-input.module.css';
 
 // TextField 스타일링
-const StyledTextField = styled(TextField)({
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#d9d9d9',
+    },
+    '&:hover fieldset': {
+      borderColor: '#d9d9d9',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'black',
+    },
+  },
   '& .MuiInputBase-root': {
+    height: '48px',
+    borderRadius: '8px',
     border: 'none',
-    color: 'red',
+  },
+  '& .MuiSvgIcon-root': {
+    position: 'fixed',
+    left: '9px',
+    color: '#9fa6b2',
   },
   '& .MuiInputBase-input': {
     fontFamily: "'Pretendard', sans-serif",
     fontWeight: 400,
     fontSize: '16px',
     color: '#171717',
-    padding: '12px 16px',
+    padding: '9px 16px 9px 40px',
+    '&::placeholder': {
+      color: '#9fa6b2',
+      fontWeight: 400,
+      fontSize: '16px',
+      fontFamily: "'Pretendard', sans-serif",
+      opacity: '1',
+    },
   },
-});
+  [theme.breakpoints.down(743)]: {
+    // 모바일 환경
+    '& .MuiInputBase-input': {
+      fontSize: '14px',
+    },
+    '& .MuiInputBase-input::placeholder': {
+      fontSize: '14px',
+    },
+  },
+}));
 
 export default function DeadlineInput() {
   const [selectedDate, setSelectedDate] = useState(null);

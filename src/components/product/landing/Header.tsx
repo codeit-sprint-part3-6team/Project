3f39d21b4e7f38141357e773e5545a5719e650c1
whdjh <whdjh98@naver.com>
2024-12-13 20:35:59 +0900

    ✨ Header완료
    모바일사이즈에서 logo부분에서 소수점이 있는데 요구사항이 소수점이라 사용했습니다 + 로고 색상 적용이 안되서 일단 흰색 로고 받아와서 했습니다!

---
+++ src/components/product/landing/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css';
+import NavBar from './NavBar';
+
+export default function Header() {
+  return (
+    <header className={styles.container}>
+      <NavBar />
+    </header>
+  );
+}
---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/product/landing/Header';
+import styles from './LandingPage.module.css';
+
+export default function LandingPage() {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+}

192a7b48e0cb99615fc406b74149106eecd8f202
ksm <hpk5802@github.com>
2024-12-13 13:03:25 +0900

    📦 SVGR 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Codeit from 'public/ic/ic_codeit.svg';
+
 export default function Home() {
-  return <>중급 프로젝트-taskify</>;
+  return (
+    <>
+      중급 프로젝트-taskify
+      <br />
+      <Codeit width={50} height={50} />
+    </>
+  );
 }

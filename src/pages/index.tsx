07c3000d04ef4f6317624cacc0211bd26888b6d3
twtwkim <alfog23@gmail.com>
2024-12-14 21:59:55 +0900

    🔥 : 테스트 코드 삭제제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import CommentInput from '@/components/common/input/CommentInput';
-import DeadlineInput from '@/components/common/input/DeadlineInput';
-import TestTitleTagInput from '@/components/common/input/TestTitleTagInput';
 import Codeit from 'public/ic/ic_codeit.svg';
 
 export default function Home() {
   return (
     <>
       중급 프로젝트-taskify
-      <CommentInput />
-      <TestTitleTagInput />
-      <DeadlineInput />
       <br />
       <Codeit width={50} height={50} />
     </>

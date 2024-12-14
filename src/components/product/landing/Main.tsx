34392eb08761d98b852f1f027baaa8ccda1dc4f2
whdjh <whdjh98@naver.com>
2024-12-14 16:56:42 +0900

    ✨ Main point2완성

--- src/components/product/landing/Main.tsx
+++ src/components/product/landing/Main.tsx
@@ -1,5 +1,5 @@
 import Landingimage2 from 'public/images/img_landingimage2.svg';
-// import Landingimage3 from 'public/images/img_landingimage3.svg';
+import Landingimage3 from 'public/images/img_landingimage3.svg';
 import styles from './Main.module.css';
 
 export default function Module() {
@@ -11,11 +11,27 @@ export default function Module() {
             <h1 className={styles.main_title}>Point 1</h1>
             <p className={styles.main_explanation}>
               일의 우선순위를
-              <br /> 관리하세요
+              <br />
+              관리하세요
             </p>
           </div>
           <div className={styles.main_imgsection}>
             <Landingimage2 className={styles.main_image} />
+          </div>
+        </div>
+      </section>
+      <section className={styles.main_reverse_section}>
+        <div className={styles.main_reverese_contentsection}>
+          <div className={styles.main_imgsection}>
+            <Landingimage3 className={styles.main_reverse_img} />
+          </div>
+          <div className={styles.main_reverse_text}>
+            <h1 className={styles.main_reverse_title}>Point 2</h1>
+            <p className={styles.main_reverse_explanation}>
+              해야 할 일을
+              <br />
+              등록하세요
+            </p>
           </div>
         </div>
       </section>

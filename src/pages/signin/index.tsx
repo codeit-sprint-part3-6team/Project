ffc5906d4fc5ddc9f0a01b531563ee954ececa50
Hwichul Shin <shinhwiiron@gmail.com>
2024-12-16 15:44:50 +0900

    🎨 회원가입 페이지 링크 추가

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,8 +1,15 @@
-import AuthInput from '@/components/common/input/AuthInput';
+import { ChangeEvent, FocusEvent, FormEvent, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 import styles from './index.module.css';
 import Logo from 'public/images/img_signinlogo.svg';
-import { ChangeEvent, useState } from 'react';
+import AuthInput from '@/components/common/input/AuthInput';
 import CDSButton from '@/components/common/button/CDSButton';
+import { ERROR_MESSAGE, PLACEHOLDER } from '@/constants/messages';
+import { emailValidation, passwordValidation } from '@/utils/authValidation';
+import OverlayContainer from '@/components/common/modal/overlay-container/OverlayContainer';
+import AuthModal from '@/components/common/modal/auth/AuthModal';
+import { postSignin } from '@/lib/signin/postSignin';
 
 const INITIAL_VALUES = {
   email: '',
@@ -11,6 +18,24 @@ const INITIAL_VALUES = {
 
 function SignIn() {
   const [values, setValues] = useState(INITIAL_VALUES);
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [responseMessage, setResponseMessage] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    const isEmailValid = emailValidation(values.email);
+    const isPasswordValid = passwordValidation(values.password);
+    setDisabled(!(isEmailValid && isPasswordValid));
+  }, [values]);
+
+  useEffect(() => {
+    if (sessionStorage.getItem('accessToken')) {
+      router.push('/mydashboard');
+    }
+  }, []);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -21,8 +46,35 @@ function SignIn() {
     }));
   };
 
-  const handleClick = () => {
-    console.log('로그인');
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name === 'email') {
+      setEmailValid(!emailValidation(value));
+    } else if (name === 'password') {
+      setPasswordValid(!passwordValidation(value));
+    }
+  };
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await postSignin(values);
+      console.log(response.accessToken);
+      sessionStorage.setItem('accessToken', response.accessToken);
+      router.push('/mydashboard');
+    } catch (error: any) {
+      if (error?.message) {
+        setResponseMessage(error.message);
+      }
+      setIsModalVisible(true);
+    }
+  };
+
+  const handleCancelClick = () => {
+    setIsModalVisible(false);
+    setResponseMessage(null);
   };
 
   return (
@@ -34,16 +86,20 @@ function SignIn() {
 
         <p className={styles.text}>오늘도 만나서 반가워요!</p>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <AuthInput
             name="email"
             htmlFor="email"
             title="이메일"
             id="email"
             type="text"
-            placeholder="이메일을 입력해 주세요"
+            placeholder={PLACEHOLDER.EMAIL_PLACEHOLDER}
             value={values.email}
             onChange={handleChange}
+            onBlur={handleBlur}
+            error={emailValid}
+            errorMessage={ERROR_MESSAGE.EMAIL_INVALID_FORMAT}
+            autoComplete="email"
           />
 
           <AuthInput
@@ -55,15 +111,32 @@ function SignIn() {
             placeholder="비밀번호를 입력해 주세요"
             value={values.password}
             onChange={handleChange}
+            onBlur={handleBlur}
+            error={passwordValid}
+            errorMessage={ERROR_MESSAGE.PASSWORD_MIN_LENGTH}
+            autoComplete="password"
           />
 
           <div className={styles.login_button}>
-            <CDSButton btnType="auth" onClick={handleClick}>
+            <CDSButton btnType="auth" type="submit" disabled={disabled}>
               로그인
             </CDSButton>
           </div>
         </form>
+
+        <p className={styles.signupText}>
+          회원이 아니신가요? <Link href={'/signup'}>회원가입하기</Link>
+        </p>
       </div>
+
+      {isModalVisible && (
+        <OverlayContainer>
+          <AuthModal
+            message={responseMessage}
+            handleCancelClick={handleCancelClick}
+          />
+        </OverlayContainer>
+      )}
     </>
   );
 }

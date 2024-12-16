701795576497ae9757235e2ba58a90b28f26aa5f
Hwichul Shin <shinhwiiron@gmail.com>
2024-12-16 15:32:06 +0900

    📝 에러메시지, placeholder 상수화

---
+++ src/constants/messages.ts
@@ -0,0 +1,8 @@
+export const ERROR_MESSAGE = {
+  EMAIL_INVALID_FORMAT: '이메일 형식으로 작성해 주세요.',
+  PASSWORD_MIN_LENGTH: '8자 이상 입력해 주세요.',
+};
+
+export const PLACEHOLDER = {
+  EMAIL_PLACEHOLDER: '이메일을 입력해 주세요',
+};

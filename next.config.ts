acb0d2d72c6440738e1411edb72c25fe8c2d9ce0
ksm <hpk5802@github.com>
2024-12-16 16:14:09 +0900

    📝 Next 이미지 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,16 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig;

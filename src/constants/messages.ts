f4070a56b92aa6058da9da826e28a9dd13fb5a26
Hwichul Shin <shinhwiiron@gmail.com>
2024-12-17 14:54:09 +0900

    📝 메시지 상수화
    placeholder, 에러 메시지 추가

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,8 +1,15 @@
 export const ERROR_MESSAGE = {
   EMAIL_INVALID_FORMAT: '이메일 형식으로 작성해 주세요.',
   PASSWORD_MIN_LENGTH: '8자 이상 입력해 주세요.',
+  NICKNAME_MIN_LENGTH: '열 자 이하로 작성해 주세요.',
+  PASSWORD_MISMATCH: '비밀번호가 일치하지 않습니다.',
 };
 
 export const PLACEHOLDER = {
   EMAIL_PLACEHOLDER: '이메일을 입력해 주세요',
+  PASSWORD_PLACEHOLDER: '비밀번호를 입력해 주세요',
+  NICKNAME_PLACEHOLDER: '닉네임을 입력해 주세요',
+  PASSWORD_LENGTH_PLACEHOLDER: '8자 이상 입력해 주세요',
+  PASSWORD_CONFIRM_PLACEHOLDER: '비밀번호를 한번 더 입력해 주세요',
+  TERMS_PLACEHOLDER: '이용약관에 동의해 주세요',
 };

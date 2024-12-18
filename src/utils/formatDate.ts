f8ffebe28367b4fdebe6d03774f9a939e8c09d9a
ksm <hpk5802@github.com>
2024-12-18 16:24:28 +0900

    ♻️ 카드 이미지 & 날짜 포매팅 유틸 함수 리팩토링

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,17 @@
-const formatDate = (date: string) => {
-  const formated = date.split(' ')[0].replaceAll('-', '.');
-  return formated;
+const formatDate = (date: string, includeTime: boolean = false): string => {
+  const dateObj = new Date(date);
+  const year = dateObj.getFullYear();
+  const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+  const day = String(dateObj.getDate()).padStart(2, '0');
+  let formattedDate = `${year}.${month}.${day}`;
+
+  if (includeTime) {
+    const hours = String(dateObj.getHours()).padStart(2, '0');
+    const minutes = String(dateObj.getMinutes()).padStart(2, '0');
+    formattedDate += ` ${hours}:${minutes}`;
+  }
+
+  return formattedDate;
 };
 
 export default formatDate;

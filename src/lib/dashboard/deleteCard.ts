068036cb065777384f37044eba20b3beab3e5461
ksm <hpk5802@github.com>
2024-12-18 21:08:03 +0900

    🩹 deleteCard API 수정

--- src/lib/dashboard/deleteCard.ts
+++ src/lib/dashboard/deleteCard.ts
@@ -2,9 +2,7 @@ import axios from '@/lib/instance';
 
 const deleteCard = async (cardId: number) => {
   try {
-    const response = await axios.delete(`/11-6/cards/${cardId}`);
-
-    return response.data;
+    await axios.delete(`/11-6/cards/${cardId}`);
   } catch (error) {
     console.error('카드 삭제 실패:', error);
     throw error;

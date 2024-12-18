5eadffad2ed4814bbad3ebaf8127c04fef555695
Hwichul Shin <shinhwiiron@gmail.com>
2024-12-18 18:48:01 +0900

    🎨 해상도별 멤버  노출 수 변경

--- src/components/common/invitedmember/InvitedMember.tsx
+++ src/components/common/invitedmember/InvitedMember.tsx
@@ -1,4 +1,4 @@
-import { Key } from 'react';
+import { Key, useEffect, useState } from 'react';
 import styles from './InvitedMember.module.css';
 
 import Avatar from '@mui/joy/Avatar';
@@ -11,14 +11,32 @@ type memberProps = {
 };
 
 function InvitedMember({ invitedMember }) {
-  const visibleAvatars = 4;
-  const overflowThreshold = 4;
+  const [count, setCount] = useState<number>(4);
+  const visibleAvatars = count;
+  const overflowThreshold = count;
+
+  useEffect(() => {
+    const updatePageSize = () => {
+      if (window.innerWidth >= 1199) {
+        setCount(4); // 데스크탑
+      } else if (window.innerWidth >= 743) {
+        setCount(3); // 태블릿
+      } else {
+        setCount(2); // 모바일
+      }
+    };
+
+    updatePageSize();
+    window.addEventListener('resize', updatePageSize);
+
+    return () => window.removeEventListener('resize', updatePageSize);
+  }, []);
 
   return (
     <>
       {invitedMember.length !== 0 && (
         <div className={styles['invited-member']}>
-          <AvatarGroup className={styles.test}>
+          <AvatarGroup className={styles['avatar-group']}>
             {invitedMember
               .slice(0, visibleAvatars)
               .map((member: memberProps) => (

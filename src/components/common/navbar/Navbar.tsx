10cb2b02189c27947dc46800b243d4a1c63cb56c
Hwichul Shin <shinhwiiron@gmail.com>
2024-12-18 16:00:00 +0900

    🚧 Navbar 작업 중
    데이터 요청 코드 분리 예정

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,39 +1,81 @@
-import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import UserProfile from '../userprofile/UserProfile';
+import Link from 'next/link';
+import instance from '@/lib/instance';
 import styles from './Navbar.module.css';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import NavButton from '../button/NavButton';
+import UserProfile from '../userprofile/UserProfile';
+import InvitedMember from '../invitedmember/InvitedMember';
 
-type NavbarProps = {
-  title: string;
-};
-
-function Navbar({ title }: NavbarProps) {
+function Navbar() {
+  const [dashboardTitle, setDashboardTitle] = useState('');
+  const [invitedMember, setInvitedMember] = useState([]);
   const user = useSelector((state: RootState) => state.userInfo.user);
   const [clientUser, setClientUser] = useState(null);
+  const router = useRouter();
+
+  const isMyDashboard = router.pathname !== '/mydashboard';
+  const isMyPage = router.pathname !== '/mypage';
+  const isEdit = router.pathname !== '/edit';
 
   useEffect(() => {
     setClientUser(user);
   }, [user]);
 
+  useEffect(() => {
+    if (!router.query.id) {
+      return;
+    }
+
+    const fetchDashboardData = async () => {
+      const membersResponse = await instance.get(
+        `11-6/members?dashboardId=${router.query.id}`,
+      );
+      setInvitedMember(membersResponse.data.members);
+
+      const titleResponse = await instance.get(
+        `11-6/dashboards/${router.query.id}`,
+      );
+      setDashboardTitle(titleResponse.data.title);
+    };
+
+    fetchDashboardData();
+  }, [router.query.id]);
+
   return (
     <div className={styles.navbar}>
-      <h3 className={styles.title}>{title}</h3>
+      {!dashboardTitle ? (
+        <h3 className={styles.title}>내 대시보드</h3>
+      ) : (
+        <h3 className={styles.title}>{dashboardTitle}</h3>
+      )}
 
       <div className={styles['right-nav']}>
-        <NavButton
-          btnType="management"
-          buttonName="관리"
-          onClick={() => alert('관리')}
-        />
-        <NavButton
-          btnType="invite"
-          buttonName="초대하기"
-          onClick={() => alert('초대하기')}
-        />
+        {isMyDashboard && isMyPage && (
+          <div className={styles['left-section']}>
+            {isEdit && (
+              <NavButton
+                btnType="management"
+                buttonName="관리"
+                onClick={() =>
+                  router.push(`/dashboards/${router.query.id}/edit`)
+                }
+              />
+            )}
 
-        <div className={styles['user-profile']}>
+            <NavButton
+              btnType="invite"
+              buttonName="초대하기"
+              onClick={() => alert('초대하기')}
+            />
+
+            <InvitedMember invitedMember={invitedMember} />
+          </div>
+        )}
+
+        <Link href={'/mypage'}>
           {clientUser && (
             <UserProfile
               type="header"
@@ -41,7 +83,7 @@ function Navbar({ title }: NavbarProps) {
               profileImageUrl={clientUser.profileImageUrl}
             />
           )}
-        </div>
+        </Link>
       </div>
     </div>
   );

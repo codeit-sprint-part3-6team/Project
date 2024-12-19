5e2fc990c81b6179eb0db32bfef02f9b6f68357c
twtwkim <alfog23@gmail.com>
2024-12-19 15:34:22 +0900

    🎨 : mypage 네비바 수정 완료

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,6 +1,6 @@
 import Sidebar from '@/components/common/sidebar/Sidebar';
 import MyPageMain from '@/components/product/mypage/Main';
-import NavBar from '@/components/product/landing/NavBar';
+import Navbar from '@/components/common/navbar/Navbar';
 import styles from './index.module.css';
 
 export default function MyPage() {
@@ -8,8 +8,10 @@ export default function MyPage() {
     <div className={styles.container}>
       <Sidebar />
       <div className={styles.space}>
-        <NavBar />
-        <MyPageMain />
+        <Navbar />
+        <div className={styles.box}>
+          <MyPageMain />
+        </div>
       </div>
     </div>
   );

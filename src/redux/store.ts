ab18bb60c0f2e78698a5254205222abd7c28a22b
Hwichul Shin <shinhwiiron@gmail.com>
2024-12-19 14:04:20 +0900

    🐛 import 에러 수정

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { UserInfoState } from '@/type/userInfo';
+import { DashboardState } from '@/type/dashboard';
 import userInfoReducer from './settingSlice';
 import dashboardReducer from './dashboardSlice';
-import { DashboardState } from '@/type/dashboard';
 
 // 최초 세션 스토리지에서 상태를 로드하는 함수
 const loadState = () => {

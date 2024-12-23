57faea90565a028849787409f66ff784dc04a5ae
Hwichul Shin <shinhwiiron@gmail.com>
2024-12-23 21:59:05 +0900

    📝 배포 확인 코드 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,8 @@ import Header from '@/components/product/landing/Header';
 import Main from '@/components/product/landing/Main';
 import Footer from '@/components/product/landing/Footer';
 import useAuthRedirect from '@/hooks/useAuthRedirect';
-import { useEffect } from 'react';
 
 export default function Home() {
-  // 배포 확인 테스트
-  useEffect(() => {
-    const url = window.location.href;
-    if (url === 'https://dev-taskify.vercel.app/') {
-      console.log('develop branch');
-    } else if (url === 'https://taskify-codeit.vercel.app/') {
-      console.log('main branch');
-    }
-  }, []);
-
   const isNotRedirected = useAuthRedirect();
 
   if (!isNotRedirected) {

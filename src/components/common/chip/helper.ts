3438b2f37ebb55deb5bc95a3ea91ce0b74c509a8
ksm <hpk5802@github.com>
2024-12-23 12:09:32 +0900

    ✨ 태그 색상 고정되도록 수정

--- src/components/common/chip/helper.ts
+++ src/components/common/chip/helper.ts
@@ -7,10 +7,16 @@ import { bgTag } from '@/type/chip';
  * @param {Record<string, string>} styles - 스타일 객체
  * @returns {string} 랜덤 배경색 클래스 이름
  */
-const getTagColor = (styles: Record<string, string>): string => {
+// const getTagColor = (styles: Record<string, string>): string => {
+//   if (!bgTag || bgTag.length === 0) return '';
+//   const idx = Math.floor(Math.random() * bgTag.length);
+//   return styles[bgTag[idx]];
+// };
+
+const getTagColor = (): string => {
   if (!bgTag || bgTag.length === 0) return '';
   const idx = Math.floor(Math.random() * bgTag.length);
-  return styles[bgTag[idx]];
+  return bgTag[idx];
 };
 
 export default getTagColor;
--- src/components/product/dashboard/card/TagManager.tsx
+++ src/components/product/dashboard/card/TagManager.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Chip from '@/components/common/chip/Chip';
 import DeleteButton from 'public/ic/ic_x.svg';
 import TitleTagInput from '@/components/common/input/info-input/TitleTagInput';
+import getTagColor from '@/components/common/chip/helper';
 import styles from './TagManager.module.css';
 
 interface TagManagerProps {
@@ -17,8 +18,11 @@ export default function TagManager({
 }: TagManagerProps) {
   const handleTagInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newTag = e.target.value.trim();
+    const color = getTagColor();
+    const coloredTag = `${newTag}^${color}`;
+
     if (!newTag || tags.includes(newTag)) return; // 빈 문자열 또는 중복 태그 방지
-    onAddTag(newTag);
+    onAddTag(coloredTag);
     e.target.value = '';
   };
 
@@ -41,19 +45,22 @@ export default function TagManager({
         required
       />
       <div className={styles.tags}>
-        {tags.map((tag) => (
-          <Chip key={`_${tag}`} chipType="tag">
-            <p className={styles.tag}>
-              {tag}
-              <DeleteButton
-                className={styles[`delete-button`]}
-                width={14}
-                height={14}
-                onClick={() => onRemoveTag(tag)}
-              />
-            </p>
-          </Chip>
-        ))}
+        {tags.map((tag) => {
+          const [tagText, tagColor] = tag.split('^');
+          return (
+            <Chip key={`_${tag}`} chipType="tag" color={tagColor}>
+              <p className={styles.tag}>
+                {tagText}
+                <DeleteButton
+                  className={styles[`delete-button`]}
+                  width={14}
+                  height={14}
+                  onClick={() => onRemoveTag(tag)}
+                />
+              </p>
+            </Chip>
+          );
+        })}
       </div>
     </div>
   );

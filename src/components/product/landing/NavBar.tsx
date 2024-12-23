d0a72c115305217cfc71940295b3b6da3b2c58aa
whdjh <whdjh98@naver.com>
2024-12-23 15:29:05 +0900

    💄 Landing페이지 class 컨벤션 수정

--- src/components/product/landing/NavBar.tsx
+++ src/components/product/landing/NavBar.tsx
@@ -5,15 +5,15 @@ import styles from './NavBar.module.css';
 
 export default function NavBar() {
   return (
-    <nav className={styles.nav_container}>
-      <div className={styles.nav_section}>
+    <nav className={styles['nav-container']}>
+      <div className={styles['nav-section']}>
         <div className={styles.navlogo_container}>
           <Link href="/">
             <Whitelogo className={styles.logo} />
           </Link>
           <Whitetextlogo className={styles.textlogo} />
         </div>
-        <div className={styles.link_container}>
+        <div className={styles['link-container']}>
           <Link href="/signin">로그인</Link>
           <Link href="/signup">회원가입</Link>
         </div>

9782ccfe98ac15bd9add6558da758488c9bd9c3e
whdjh <whdjh98@naver.com>
2024-12-24 11:46:13 +0900

    💄 가로 뷰포트 넘치면 overflowX로 감춤

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
         width: '100%',
         height: '100%',
         backgroundColor: 'var(--black)',
+        overflowX: 'hidden',
       }}
     >
       <Header />

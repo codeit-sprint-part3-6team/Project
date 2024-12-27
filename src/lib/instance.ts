7802deea533c861e8d8c45546fb80ba62fc3430b
ksm <hpk5802@github.com>
2024-12-27 16:02:35 +0900

    ✨ 401 에러 발생 시 로그인 페이지로 리다이렉트

--- src/lib/instance.ts
+++ src/lib/instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Router from 'next/router';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -17,5 +18,34 @@ instance.interceptors.request.use((config) => {
   }
   return config;
 });
+
+let isRedirecting = false; // 리다이렉트 종료 확인을 위한 flag
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    /**
+     * response.status가 401(UnAuthorized)이면
+     * 스토리지 토큰 삭제 후 로그인(/signin) 페이지로 리다이렉트
+     */
+    if (error.response?.status === 401) {
+      if (!isRedirecting) {
+        isRedirecting = true;
+
+        localStorage.removeItem('accessToken');
+        sessionStorage.removeItem('accessToken');
+
+        alert('로그인이 필요한 서비스 입니다.\n로그인 페이지로 이동합니다.');
+        await Router.push('/signin');
+
+        isRedirecting = false;
+      }
+
+      return new Promise(() => {}); // API 요청 실패 에러 처리 방지
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default instance;

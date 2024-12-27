ff6be56601c67ab149b6c1e07cf809ad6634685e
Hwichul Shin <shinhwiiron@gmail.com>
2024-12-27 22:13:30 +0900

    📝 outer export 추가

--- next.config.ts
+++ next.config.ts
@@ -18,8 +18,11 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+
+  output: 'export',
   images: {
     domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'], // 외부 이미지 도메인 추가
+    unoptimized: true, // 이미지 최적화 비활성화
   },
 };
 

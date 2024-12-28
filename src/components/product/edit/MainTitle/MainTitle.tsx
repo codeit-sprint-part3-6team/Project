96f839a0db6e3093fae929f32fec848624f06a5a
whdjh <whdjh98@naver.com>
2024-12-28 15:47:41 +0900

    💄 변경하기버튼 클릭시 바로 변경
    대시보드에서 타이틀변경도 바로 변경되어 똑같이 적용했습니다.

--- src/components/product/edit/MainTitle/MainTitle.tsx
+++ src/components/product/edit/MainTitle/MainTitle.tsx
@@ -8,42 +8,6 @@ import useSidebarDashboards from '@/hooks/useSidebar';
 import ColorSelector from './ColorSelector';
 import styles from './MainTitle.module.css';
 
-function EditConfirmModal({
-  isOpen,
-  onClose,
-  onConfirm,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-  onConfirm: () => void;
-}) {
-  if (!isOpen) {
-    return null;
-  }
-
-  const handleContainerClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === e.currentTarget) {
-      onClose();
-    }
-  };
-
-  return (
-    <div className={styles['modal-container']} onClick={handleContainerClick}>
-      <div className={styles['modal-section']}>
-        <h3>제목 또는 색상을 변경하시겠습니까?</h3>
-        <div className={styles['modal-button']}>
-          <button onClick={onClose} className={styles['cancel-button']}>
-            취소
-          </button>
-          <button onClick={onConfirm} className={styles['confirm-button']}>
-            확인
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
 interface MainTitleProps {
   dashboardtitle: string | null;
   dashboardColor: string | null;
@@ -56,21 +20,12 @@ export default function MainTitle({
   const [selectedColor, setSelectedColor] = useState<string>(dashboardColor);
   const [title, setTitle] = useState(dashboardtitle || '');
   const [newTitle, setNewTitle] = useState(dashboardtitle || '');
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
   const dashboardId = Number(router.query.id);
   const { fetchSidebarDashboards } = useSidebarDashboards();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
-  };
-
-  const handleEditClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
   };
 
   const confirmEdit = async () => {
@@ -88,8 +43,6 @@ export default function MainTitle({
     } catch (error) {
       toast.error('변경에 실패했습니다.');
       throw new Error(`${error}`);
-    } finally {
-      closeModal();
     }
   };
 
@@ -118,16 +71,11 @@ export default function MainTitle({
           </div>
         </div>
         <div className={styles['sheader-button']}>
-          <CDSButton btnType="auth" onClick={handleEditClick}>
+          <CDSButton btnType="auth" onClick={confirmEdit}>
             변경
           </CDSButton>
         </div>
       </div>
-      <EditConfirmModal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        onConfirm={confirmEdit}
-      />
     </div>
   );
 }
